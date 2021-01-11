41a59197fd516a6bdca3a526f1acd52873e7511c
DanetteCA <77286320+DanetteCA@users.noreply.github.com>
2021-01-11 17:41:10 +0200

    Add files via upload

---
+++ src/SammysMotto.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Danette
+ */
+public class SammysMotto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("*************************************");
+        System.out.println("** Sammy's makes it fun in the sun **");
+        System.out.println("*************************************");
+    }
+    
+}
